c8c437e5b9a45a00aa88c711071a964636457a27
alisoneoz <alisonestephanyoz@gmail.com>
2023-10-23 13:27:16 -0400

    Add style

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import randomFunction from "./components/jsComponents/jscomponent"
 function App() {
 
   return (
-    <div className="">
-      hola
-      <button onClick={randomFunction} className="bg-green-500 p-5">Fire the JS function</button>
+    <div className="flex flex-col m-5 justify-center">
+      <h1 className="font-bold text-3xl">hola</h1>
+      <button onClick={randomFunction} className="bg-green-500 p-5 w-fit mt-5 font-semibold rounded-lg hover:bg-green-400">Fire the JS function</button>
     </div>
   )
 }
